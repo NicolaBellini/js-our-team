f50c591fe68e8fe16bc7e2ffe1abede3e94b8093
NicolaBellini <bellininicola4366@gmail.com>
2024-03-05 14:36:28 +0100

    [add] bonus1

--- js/script.js
+++ js/script.js
@@ -2,47 +2,47 @@
 const output = document.querySelector(".output")
 
 
-
+// milestone0
 const members = [
    {
       completeName:"Wayne Barnett",
       role:"Founder & CEO",
-      img:"wayne-barnett-founder-ceo.jpg",
+      img:"assets/img/wayne-barnett-founder-ceo.jpg",
    },
    {
       completeName:"Angela Caroll",
       role:"Chief Editor	",
-      img:"angela-caroll-chief-editor.jpg",
+      img:"assets/img/angela-caroll-chief-editor.jpg",
    },
    {
       completeName:"Walter Gordon",
       role:"Office Manager",
-      img:"wayne-barnett-founder-ceo.jpg",
+      img:"assets/img/walter-gordon-office-manager.jpg",
    },
    {
       completeName:"Angela Lopez",
       role:"Social Media Manager",
-      img:"angela-lopez-social-media-manager.jpg",
+      img:"assets/img/angela-lopez-social-media-manager.jpg",
    },
    {
       completeName:"Scott Estrada",
       role:"Developer	",
-      img:"scott-estrada-developer.jpg",
+      img:"assets/img/scott-estrada-developer.jpg",
    },
    {
       completeName:"Barbara Ramos",
       role:"Graphic Designe",
-      img:"barbara-ramos-graphic-designer.jpg",
+      img:"assets/img/barbara-ramos-graphic-designer.jpg" ,
    },
 ]
 
 // milestone1
 for(let member of members){
    console.log(member);
-   // milestone2
+   // milestone2 e bonus1
    output.innerHTML+=`
    <li>
-      <p>Name: ${member.completeName} - Role: ${member.role} - img:${member.img} </p>
+      <p>Name: ${member.completeName} - Role: ${member.role} - <img src="${member.img}" title="${member.completeName}" alt="${member.completeName}"> </p>
    </li>
    `
 }
