67296676e94ff0b2cb9033d4b5b8b4ab5ed08573
NicolaBellini <bellininicola4366@gmail.com>
2024-03-05 14:23:10 +0100

    [add] members array

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+const members = [
+   {
+      completeName:"Wayne Barnett",
+      role:"Founder & CEO",
+      img:"wayne-barnett-founder-ceo.jpg",
+   },
+   {
+      completeName:"Angela Caroll",
+      role:"Chief Editor	",
+      img:"angela-caroll-chief-editor.jpg",
+   },
+   {
+      completeName:"Walter Gordon",
+      role:"Office Manager",
+      img:"wayne-barnett-founder-ceo.jpg",
+   },
+   {
+      completeName:"Angela Lopez",
+      role:"Social Media Manager",
+      img:"angela-lopez-social-media-manager.jpg",
+   },
+   {
+      completeName:"Scott Estrada",
+      role:"Developer	",
+      img:"scott-estrada-developer.jpg",
+   },
+   {
+      completeName:"Barbara Ramos",
+      role:"Graphic Designe",
+      img:"barbara-ramos-graphic-designer.jpg",
+   },
+]
+
+
+
+console.log(members[3].completeName)
+
